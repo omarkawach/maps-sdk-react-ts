4ed7e826ed0b7788235129700accd3b7f39590df
Omar Kawach <omarkawach@outlook.com>
2025-02-07 12:49:20 -0800

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+// Individual imports for each component used in this sample
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/dist/components/arcgis-legend";
+import "@arcgis/map-components/dist/components/arcgis-search";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <arcgis-map
+      itemId="d5dda743788a4b0688fe48f43ae7beb9"
+      onarcgisViewReadyChange={(event) => {
+        console.log("MapView ready", event);
+      }}
+    >
+      <arcgis-search position="top-right"></arcgis-search>
+      <arcgis-legend position="bottom-left"></arcgis-legend>
+    </arcgis-map>
+  </StrictMode>
+);
